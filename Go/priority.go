937f5c7e18beca78dc9c61c6ba2b7e15f44b01db
shraman <pkpaulpostdn@gmail.com>
2024-04-27 17:54:02 +0530

    check 96

--- Go/priority.go
+++ Go/priority.go
@@ -93,7 +93,7 @@ func Priority() {
 		}
 
 		feeToWeightRatio := float64(CalculateFee(tx)) / float64(CalculateWeight(tx))
-		if feeToWeightRatio >= 3.0 && CalculateWeight(tx) < 4000 {
+		if feeToWeightRatio >= 3.0 && CalculateWeight(tx) < 4500 {
 			count++
 			ratio = append(ratio, feeToWeightRatio)
 			// fmt.Println("Ratio: ", feeToWeightRatio)
